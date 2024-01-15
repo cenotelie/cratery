fc0242ceec42b464d65ce1c8843425bed2ae9e42
Laurent Wouters <lwouters@cenotelie.fr>
2024-01-15 15:17:31 +0100

    Activating deploy script

--- build-deploy.sh
+++ build-deploy.sh
@@ -5,10 +5,10 @@ ROOT="$(dirname "$SCRIPT")"
 
 source "$ROOT/build-env.sh"
 
-# curl -X POST \
-#         -H "Authorization: Bearer $DEPLOY_CREDENTIALS" \
-#         -H 'Content-Type: application/json' \
-#         -d "{\"message\": \"Deploy Cratery $DOCKER_TAG\", \"args\": [\"$DOCKER_TAG\"]}" \
-#         "https://deploy.cenotelie.fr/actions/public/deploy-cargo.sh"
+curl -X POST \
+        -H "Authorization: Bearer $DEPLOY_CREDENTIALS" \
+        -H 'Content-Type: application/json' \
+        -d "{\"message\": \"Deploy Cratery $DOCKER_TAG\", \"args\": [\"$DOCKER_TAG\"]}" \
+        "https://deploy.cenotelie.fr/actions/public/deploy-cratery.sh"
 
 echo ""
