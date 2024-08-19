20b0675c6c29255e4b902c6f3507b757532f467e
Marc-Antoine Arnaud <marc-antoine.arnaud@luminvent.com>
2024-08-19 23:09:38 +0200

    refactor: use unique sha256 method

---
+++ src/utils/hashes.rs
@@ -0,0 +1,10 @@
+use data_encoding::HEXLOWER;
+use ring::digest::{Context, SHA256};
+
+/// Computes the SHA256 digest of bytes
+pub fn sha256(buffer: &[u8]) -> String {
+  let mut context = Context::new(&SHA256);
+  context.update(buffer);
+  let digest = context.finish();
+  HEXLOWER.encode(digest.as_ref())
+}
