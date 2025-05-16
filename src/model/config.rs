29d3b08a7a409c5ab265307cf5aaa92b774e03b5
Gwen Lg <me@gwenlg.fr>
2025-05-16 23:04:38 +0200

    feat(config): manage .cargo creation directory if not exist

--- src/model/config.rs
+++ src/model/config.rs
@@ -14,7 +14,7 @@ use base64::Engine;
 use base64::engine::general_purpose::STANDARD;
 use serde_derive::{Deserialize, Serialize};
 use thiserror::Error;
-use tokio::fs::File;
+use tokio::fs::{self, File};
 use tokio::io::{self, AsyncWrite, AsyncWriteExt, BufWriter};
 use tokio::process::Command;
 
@@ -26,6 +26,16 @@ use crate::utils::token::generate_token;
 
 #[derive(Debug, Error)]
 pub enum WriteConfigError {
+    #[error("failed to compute parent path for '{path}'")]
+    ParentPath { path: PathBuf },
+
+    #[error("failed to create folder '{path}'")]
+    CreateDir {
+        #[source]
+        source: io::Error,
+        path: PathBuf,
+    },
+
     #[error("failed to create file '{path}'")]
     CreateConfigFile {
         #[source]
@@ -776,6 +786,21 @@ impl Configuration {
 
     async fn create_config_file(&self, path: &[&str]) -> Result<BufWriter<File>, WriteConfigError> {
         let path = self.get_home_path_for(path);
+
+        // create parent folder if needed
+        let parent_path = path
+            .parent()
+            .ok_or_else(|| WriteConfigError::ParentPath { path: path.clone() })?;
+        if !parent_path.exists() {
+            fs::create_dir(parent_path)
+                .await
+                .map_err(|source| WriteConfigError::CreateDir {
+                    source,
+                    path: parent_path.to_path_buf(),
+                })?;
+        }
+
+        // create config file
         let file = File::create(&path)
             .await
             .map_err(|source| WriteConfigError::CreateConfigFile { source, path })?;
